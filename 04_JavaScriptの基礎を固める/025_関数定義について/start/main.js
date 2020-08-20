3318a9c585fbe756cef62ddfb3120204b2e5d3e5
kaefan74 <gongon-loveuper.kae@ezweb.ne.jp>
2020-08-20 22:04:54 +0900

    025_関数定義について :wq

---
+++ 04_JavaScriptの基礎を固める/025_関数定義について/start/main.js
@@ -0,0 +1,9 @@
+
+const hello = function hello(name,age){
+  // let name = 'Code';
+  // console.log('name'+name+age);
+  return name+age;
+}
+hello('Code',10);
+const re = hello('Code2',20);
+console.log(re);
