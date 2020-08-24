2434d544220092f5069a5e74a2f65f76fe42a43f
kaefan74 <gongon-loveuper.kae@ezweb.ne.jp>
2020-08-24 21:23:14 +0900

    070_関数定義とアロー関数 :wq

---
+++ 04_JavaScriptの基礎を固める/070_関数定義とアロー関数/start/main.js
@@ -0,0 +1,9 @@
+// function hello(name){
+//   console.log('hello'+name);
+// }
+// const hello = name => console.log('hello'+name);
+// hello('Kae');
+const hello = (name,age)=>40;
+
+const arry = [1,2,3,4,5,6];
+arry.forEach( value =>console.log(value));
